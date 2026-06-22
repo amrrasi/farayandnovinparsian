8a03d48cd4363cdecddb5e3c7c8979d0f13280fe
amrrasi <amirrezaaskari2003@gmail.com>
2026-06-22 15:52:17 +0330

    add logout to header

--- cms/myadmin/inc/header.php
+++ cms/myadmin/inc/header.php
@@ -741,7 +741,7 @@
                                 </svg>
                                 <span class="ml-2">ورودی</span>
                             </a>
-                            <a href="page-login.html" class="dropdown-item ai-icon">
+                            <a href="logout.php" class="dropdown-item ai-icon">
                                 <svg id="icon-logout" xmlns="http://www.w3.org/2000/svg" class="text-danger" width="18" height="18"
                                      viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
                                      stroke-linejoin="round">
