18188286b0a2b9da0f20b6d7f48fd561d6cee7c3
amrrasi <amirrezaaskari2003@gmail.com>
2026-07-13 11:47:51 +0330

    add submit order

---
+++ ajax/cart/clearCart.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types=1);
+header('Content-Type: application/json; charset=utf-8');
+require_once "../cms/myadmin/inc/config.php";
+
+function respond(bool $ok, string $msg): void {
+    echo json_encode(['status' => $ok, 'message' => $msg], JSON_UNESCAPED_UNICODE);
+    exit;
+}
+
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+    http_response_code(405); respond(false, 'Method Not Allowed');
+}
+
+$_SESSION['cart'] = [];
+respond(true, 'سبد خرید پاک شد');
