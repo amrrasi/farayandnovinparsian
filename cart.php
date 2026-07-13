e28b4ff2ad093c46601ee8187750f3fba8f70ef3
amrrasi <amirrezaaskari2003@gmail.com>
2026-07-13 11:47:40 +0330

    add submit order

--- cart.php
+++ cart.php
@@ -1,1 +1,45 @@
 <?php
+require_once "cms/myadmin/inc/config.php";
+
+
+?>
+<!doctype html>
+<html lang="fa" dir="rtl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="<?= htmlspecialchars($_SESSION['csrf_token']) ?>">
+    <title><?=  ?></title>
+
+    <?= $global_base_address ?>
+
+    <link rel="stylesheet" href="assets/css/base/animate.min.css">
+    <link rel="stylesheet" href="assets/css/base/flaticon.css">
+    <link rel="stylesheet" href="assets/css/base/fontawesome.min.css">
+    <link rel="stylesheet" href="assets/css/base/magnific-popup.min.css">
+    <link rel="stylesheet" href="assets/css/base/nice-select.css">
+    <link rel="stylesheet" href="assets/css/base/owl.carousel.min.css">
+
+    <link rel="stylesheet" href="assets/fonts/font.css">
+
+    <link rel="stylesheet" href="assets/css/base/bootstrap.rtl.css">
+    <link rel="stylesheet" href="assets/css/base/base.css">
+    <link rel="stylesheet" href="assets/css/layout/header.css">
+    <link rel="stylesheet" href="assets/css/layout/footer.css">
+    <link rel="stylesheet" href="assets/css/pages/cart.css?v=<?= filemtime('assets/css/pages/cart.css') ?>">
+</head>
+<body class="entry-body">
+
+
+
+
+
+<script src="assets/js/jquery.js"></script>
+<script src="assets/js/jquery.nice-select.min.js"></script>
+<script src="assets/js/owl.carousel.min.js"></script>
+<script src="assets/js/bootstrap.js"></script>
+<script src="assets/js/bootstrap.bundle.js"></script>
+<script src="assets/js/main.js"></script>
+<script src="assets/js/pages/cart.js?v=<?= filemtime('assets/js/pages/cart.js') ?>"></script>
+</body>
+</html>
